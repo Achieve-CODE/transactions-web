aeb014c8a5632ef8b7a46dfd18e84411542badff
re:devised <felix.schaefer@redevised.io>
2022-08-12 09:57:50 +0200

    Added design V1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import "../app.css";
 	import haversine from 'haversine-distance';
 
 	let lat: number;
@@ -37,13 +38,29 @@
 	}
 </script>
 
-Latitude: {lat}
+<!-- Latitude: {lat}
 Longitude: {long}
-total: {total}
-steps: {total / 0.8}
+total: {total} -->
 
-{#if timeout}
-	<button on:click={stop}>stop</button>
-{:else}
-	<button on:click={walk}>start</button>
-{/if}
+<div class="min-w-screen min-h-screen h-full w-full 
+flex flex-col justify-center items-center gap-10
+bg-green-500">
+
+	<div class="text-4xl text-white border-b-2 border-white pb-10">
+		<span class="text-6xl font-bold">{total / 0.8}</span> steps
+	</div>
+
+	{#if timeout}
+		<button on:click={stop}
+		class="py-2 px-8 rounded-full text-xl
+				border-2 border-white text-white font-bold uppercase"
+		>stop</button>
+	{:else}
+		<button on:click={walk}
+		class="py-2 px-8 rounded-full text-xl
+				border-2 border-white text-white font-bold uppercase"
+		>start</button>
+	{/if}
+
+</div>
+
