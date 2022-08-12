3b0836f62c143ba826f39aa06b512d0de7199f81
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2022-08-12 11:45:42 +0200

    feat: transaction

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import "../app.css";
+	import '../app.css';
 	import haversine from 'haversine-distance';
 
 	let lat: number;
@@ -36,31 +36,53 @@
 		clearTimeout(timeout);
 		timeout = null;
 	}
+
+	function transaction() {
+		if ((total / 0.8) * 0.01 > 10) {
+			fetch(
+				`https://corsproxy.felixschaefer.access.ly/http://5.161.66.94:3000/transferToWallet?amount=${
+					(total / 0.8) * 0.01
+				}`,
+				{
+					headers: {
+						method: 'GET'
+					}
+				}
+			);
+			total = 0;
+		} else alert('Go run more.');
+	}
 </script>
 
 <!-- Latitude: {lat}
 Longitude: {long}
 total: {total} -->
 
-<div class="min-w-screen min-h-screen h-full w-full 
+<div
+	class="min-w-screen min-h-screen h-full w-full 
 flex flex-col justify-center items-center gap-10
-bg-[#F27F3D]">
-
+bg-[#F27F3D]"
+>
 	<div class="text-4xl text-white border-b-2 border-white pb-10">
-		<span class="text-6xl font-bold">{Math.round(total / 0.8)}</span> steps
+		<span class="text-6xl font-bold">{Math.floor(total / 0.8)}</span> steps
 	</div>
 
 	{#if timeout}
-		<button on:click={stop}
-		class="py-2 px-8 rounded-full text-xl
-				border-2 border-white text-white font-bold uppercase"
-		>stop</button>
+		<button
+			on:click={stop}
+			class="py-2 px-8 rounded-full text-xl
+				border-2 border-white text-white font-bold uppercase">stop</button
+		>
 	{:else}
-		<button on:click={walk}
-		class="py-2 px-8 rounded-full text-xl
-				border-2 border-white text-white font-bold uppercase"
-		>start</button>
+		<button
+			on:click={walk}
+			class="py-2 px-8 rounded-full text-xl
+				border-2 border-white text-white font-bold uppercase">start</button
+		>
+		<button
+			on:click={transaction}
+			class="py-2 px-8 rounded-full text-xl
+				border-2 border-white text-white font-bold uppercase">transaction</button
+		>
 	{/if}
-
 </div>
-
